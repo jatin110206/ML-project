60cdb727da318d7925ed8fb9b34b80b79e3c5154
jatin110206 <jatinprakashrathore@gmail.com>
2025-10-18 20:04:34 +0530

    Initial commit: Add Flask app and pipeline

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask,request,render_template 
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.pred_pipline import CustomData,PredPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+        )
+        pred_df=CustomData.get_data_as_data_frame(data)
+        print(pred_df)
+        predict_pipline=PredPipeline()
+        results=predict_pipline.Predict(pred_df)
+
+        return render_template('home.html',results=results[0])
+
+
+if __name__=="__main__":
+    app.run(debug=True)
--- src/utils.py
+++ src/utils.py
@@ -35,7 +35,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
+            gs = GridSearchCV(model,para,cv=3,scoring='r2',n_jobs=-1)
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
@@ -65,3 +65,11 @@ def load_object(file_path):
 
     except Exception as e:
         raise CustomExeption(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomExeption(e,sys)
